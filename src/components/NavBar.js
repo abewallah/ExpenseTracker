2c64158f00fb897d814f489d94a9ea3b01841a65
abewallah <ibrahimabdull93@gmail.com>
2021-05-24 17:01:39 -0700

    fixes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -24,48 +24,13 @@ function NavBar() {
         <div className='collapse navbar-collapse' id='navbarSupportedContent'>
           <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
             <li className='nav-link '>
-              <Link to='/register' className='nav-link'>
-                Register
+              <Link to='/homepage' className='nav-link'>
+                HomePage
               </Link>
             </li>
             <li className='nav-link '>
-              <Link to='/login' className='nav-link'>
-                Login
-              </Link>
-            </li>
-
-            <li className='nav-item dropdown nav-link'>
-              <a
-                className='nav-link dropdown-toggle'
-                href='#'
-                id='navbarDropdown'
-                role='button'
-                data-bs-toggle='dropdown'
-                aria-expanded='false'
-              >
-                Dropdown
-              </a>
-              <ul className='dropdown-menu' aria-labelledby='navbarDropdown'>
-                <li>
-                  <Link to='/homepage' className='dropdown-item'>
-                    HomePage
-                  </Link>
-                </li>
-                <li>
-                  <Link to='/profile' className='dropdown-item'>
-                    Profile Page
-                  </Link>
-                </li>
-                <li>
-                  <Link to='/about' className='dropdown-item'>
-                    About
-                  </Link>
-                </li>
-              </ul>
-            </li>
-            <li className='nav-link'>
-              <Link to='/' className='nav-link'>
-                Log Out
+              <Link to='/about' className='nav-link'>
+                About
               </Link>
             </li>
           </ul>
